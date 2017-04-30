ed1486aa4ef24d66f188e7f4104303c8109f84e8
Andrey Romantsev <pixel33develop@gmail.com>
2017-04-30 23:50:17 +0300

    Добавить конфиг базы для девелопа.

--- app/environment/base.py
+++ app/environment/base.py
@@ -1,4 +1,4 @@
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'development_key')
-DATABASE_URL = os.environ.get('DATABASE_URL', None)
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://bothub:123456@localhost/bothub_db')
