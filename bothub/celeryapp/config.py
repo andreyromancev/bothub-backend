099ec4adb5e9b7eafd7c3fa04a623d737f937e47
Andrey Romantsev <pixel33develop@gmail.com>
2017-05-05 00:21:09 +0300

    Пофиксить конфиг селери.

--- bothub/celeryapp/config.py
+++ bothub/celeryapp/config.py
@@ -1,17 +1,16 @@
 import os
-from django.conf import settings
 
 
-if settings.IS_DEVELOPMENT:
+redis_url = os.environ.get('REDIS_URL')
+if redis_url:
+    broker_url = redis_url
+    result_backend = redis_url
+else:
     result_backend = 'cache'
     cache_backend = 'memory'
     broker_url = 'memory://'
     task_always_eager = True
     task_eager_propagates = True
-else:
-    redis_url = os.environ.get('REDIS_URL')
-    broker_url = redis_url
-    result_backend = redis_url
 
 task_serializer = 'json'
 accept_content = ['json']
