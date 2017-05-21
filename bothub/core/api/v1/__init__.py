b63d9a74c3965b8a5c9b6d99890e1a3d1211aa21
Andrey Romantsev <pixel33develop@gmail.com>
2017-05-21 15:15:44 +0300

    Реструктуризация проекта.

--- bothub/core/api/v1/__init__.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .user import UserViewSet
-
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
-urlpatterns = router.urls
--- bothub/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-
-from .api.v1 import urlpatterns as url_v1
-
-
-urlpatterns = [
-    url(r'^v1/', include(url_v1)),
-]
